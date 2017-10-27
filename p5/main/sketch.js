582cd83452d2803a9529659ce5d44a6f107f0bad
Oscar <oscar.peterson91@gmail.com>
2017-10-27 22:28:13 +0800

    Ran beautify

--- p5/main/sketch.js
+++ p5/main/sketch.js
@@ -1,7 +1,6 @@
-
 function setup() {
   // Setting up background environment
-  createCanvas(640,360);
+  createCanvas(640, 360);
 }
 
 function draw() {
@@ -10,18 +9,18 @@ function draw() {
 
   // White rectangle with green border, border size 3
   fill(255);
-  stroke(0,255,0);
+  stroke(0, 255, 0);
   strokeWeight(3);
-  rect(100,200,75,150);
+  rect(100, 200, 75, 150);
 
   // Red ellipse with white border, border size 1
-  fill(255,0,0);
+  fill(255, 0, 0);
   stroke(255);
   strokeWeight(1);
-  ellipse(300,200,80,80);
+  ellipse(300, 200, 80, 80);
 
   // Purple line with weight 5
-  stroke(255,0,255);
+  stroke(255, 0, 255);
   strokeWeight(5);
-  line(50,50,350,350);
+  line(50, 50, 350, 350);
 }
