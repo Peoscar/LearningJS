0ce0ef4fb3e4d060481a992a8b337817ef19d684
Oscar Peterson <oscar.peterson91@gmail.com>
2018-12-22 06:46:49 +0100

    bubbles (formatting) * New formatting for atom-beautify + Reference to .ico (fix console error)

--- my_collection/bubbles/sketch.js
+++ my_collection/bubbles/sketch.js
@@ -11,54 +11,54 @@ bubbleRadius = 12;
 
 
 function setup() {
-  createCanvas(windowWidth / 2, windowHeight / 2);
-  for (let i = 0; i < MAX_BUBBLES; i++) {
-    bubbles[i] = new Bubble();
-  }
+    createCanvas(windowWidth / 2, windowHeight / 2);
+    for (let i = 0; i < MAX_BUBBLES; i++) {
+        bubbles[i] = new Bubble();
+    }
 }
 
 function draw() {
-  background(0);
+    background(0);
 
-  for (let i = 0; i < bubbles.length - 1; i++) {
-    bubbles[i].show();
-    bubbles[i].move();
-  }
+    for (let i = 0; i < bubbles.length - 1; i++) {
+        bubbles[i].show();
+        bubbles[i].move();
+    }
 }
 
 function mousePressed() {
-  for (let i = 0; i < bubbles.length - 1; i++) {
-    if (mouseX <= bubbles[i].x + bubbleRadius &&
-      mouseX >= bubbles[i].x - bubbleRadius &&
-      mouseY <= bubbles[i].y + bubbleRadius &&
-      mouseY >= bubbles[i].y - bubbleRadius) {
-      bubbles.splice(i, 1);
-      console.log("You popped one!")
+    for (let i = 0; i < bubbles.length - 1; i++) {
+        if (mouseX <= bubbles[i].x + bubbleRadius &&
+            mouseX >= bubbles[i].x - bubbleRadius &&
+            mouseY <= bubbles[i].y + bubbleRadius &&
+            mouseY >= bubbles[i].y - bubbleRadius) {
+            bubbles.splice(i, 1);
+            console.log("You popped one!")
+        }
     }
-  }
 }
 
 class Bubble {
-  constructor() {
-    this.x = random(windowWidth / 2);
-    this.y = height;
-  }
+    constructor() {
+        this.x = random(windowWidth / 2);
+        this.y = height;
+    }
 
-  // pop(index) {
-  //   if (index > -1) {
-  //     bubbles.splice(index, 1);
-  //   }
-  // }
+    // pop(index) {
+    //   if (index > -1) {
+    //     bubbles.splice(index, 1);
+    //   }
+    // }
 
-  move() {
-    this.x = this.x + random(-2, 2);
-    this.y = this.y + random(-1, 0);
-  }
+    move() {
+        this.x = this.x + random(-2, 2);
+        this.y = this.y + random(-1, 0);
+    }
 
-  show() {
-    stroke(255);
-    strokeWeight(2);
-    noFill();
-    ellipse(this.x, this.y, bubbleRadius * 2, bubbleRadius * 2);
-  }
+    show() {
+        stroke(22, 123, 211);
+        strokeWeight(2);
+        noFill();
+        ellipse(this.x, this.y, bubbleRadius * 2, bubbleRadius * 2);
+    }
 }
